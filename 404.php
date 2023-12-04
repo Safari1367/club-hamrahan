0e9bd50d4267fc15ced5c898cb0d83b15d5f2c6a
Safari1367 <maryamsafarim82@gmail.com>
2023-12-04 14:56:43 +0330

    add 404 page

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* File Name:        index.php
+* File Name:        404.php
 * Author:           Sohrab Yazdanparast <sohrab.yazdan@yahoo.com>
 * License:          Check license URI for more information
 * @Author-URI:      https://www.www.venus-itc.com.com
@@ -8,22 +8,32 @@
 * @License-URI:     https://www.venus-itc.com/license
 */
 get_header(); ?>
-<div class="row">
-    <div class="col-lg-12">
-        <main class="main-box main-container error_404">
-            <h1>
-                <?php echo __('پیدا نشد!', 'artdesign'); ?>
-            </h1>
-            <div>
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/404.png" class="img-fluid" alt="<?php bloginfo('sitename') ?>">
-                <div class="error_404_text">
-                    <?php echo __('به نظر می رسد محتوای مورد نظر شما در دسترس نمی باشد.', 'artdesgin'); ?>
-                </div>
-                <div class="error_404_btn">
-                    <a href="/"><?php echo __('صفحه اصلی', 'artdesign'); ?></a>
+<div class="content-page rtl-page">
+
+
+    <div class="container-fluid">
+        <?php
+        get_template_part('templates/content/breadcrumb');
+        ?>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card not-found-page">
+                    <div class="not-found-row">
+
+                        <div class="d-flex">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/404.png" class="img-fluid" alt="<?php bloginfo('sitename') ?>">
+                        </div>
+                        <div class="d-flex btn-box">
+
+                            <h4><?php echo __('به نظر می رسد محتوای مورد نظر شما در دسترس نمی باشد.', 'artdesgin'); ?></h4>
+
+                            <a href="/" class="btn mb-1 btn-primary"><?php echo __('صفحه اصلی', 'artdesign'); ?></a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </main>
+        </div>
     </div>
+</div>
 </div>
 <?php get_footer();
